b026ae1ba079dfbbe1f10f24373c8626b96af609
edwar <edwardsameh99@gmail.com>
2025-09-04 00:10:17 +0300

    Vid03

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -1,5 +1,8 @@
-﻿using Demo.Contexts;
+﻿using Azure;
+using Demo.Contexts;
+using Demo.Entities;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.Net;
 using System.Runtime.ConstrainedExecution;
 using System.Security.Claims;
 
@@ -47,6 +50,54 @@ namespace Demo
             // }
 
             using AppDbContext context = new AppDbContext();
+
+
+            #region Create - Insert
+            // Create - Insert
+
+
+            //var employee = new Employee()
+            //{
+            //    Name = "Ahmed Ali",
+            //    Salary = 12000,
+            //    Address = "Cairo",
+            //    Age = 25
+            //};
+
+
+            //Console.WriteLine(context.Entry(employee).State); // Detached
+
+            //employee.Name = "Khaled";
+
+            //Console.WriteLine(context.Entry(employee).State); // Detached
+            ////context.Add(employee);
+            //context.Employees.Add(employee);
+
+            //Console.WriteLine(context.Entry(employee).State); // Added
+
+            //var Result = context.SaveChanges();
+
+            //Console.WriteLine(context.Entry(employee).State); // Unchanged
+
+            //employee.Name = "Omar";
+
+            //Console.WriteLine(context.Entry(employee).State); // Modified
+
+            //Console.WriteLine(Result);
+
+            //context.Employees.Add(employee);
+            //context.SaveChanges();
+
+            //Console.WriteLine(context.Entry(employee).State);
+
+            //context.Entry(employee).State = Microsoft.EntityFrameworkCore.EntityState.Added;
+
+            //Console.WriteLine(context.Entry(employee).State);
+
+            //context.SaveChanges(); 
+            #endregion
+
+
 
         }
     }
